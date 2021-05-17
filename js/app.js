6ee83129f204c1ca29c8104d0ea2df72bb7767bd
Jonathan Ruiz <jruiz.967@gmail.com>
2021-05-16 23:36:42 -0500

    Se crearon los paginadores

--- js/app.js
+++ js/app.js
@@ -1,6 +1,11 @@
 //variables
 const formulario = document.querySelector('#formulario');
 const resultado = document.querySelector('#resultado');
+const paginacionDiv = document.querySelector('#paginacion');
+
+const registrosPagina = 20;
+let totalPaginas;
+let iterador;
 
 //funciones
 function iniciarBusqueda(e) {
@@ -17,16 +22,19 @@ function iniciarBusqueda(e) {
 
 function buscarImagenes(termino){
     const key = '21610553-9f6f08ebc7ba0f2edf70ed443';
-    const url = `https://pixabay.com/api/?key=${key}&q=${termino}&per_page=20`;
+    const url = `https://pixabay.com/api/?key=${key}&q=${termino}&per_page=${registrosPagina}`;
 
     fetch(url)
         .then(response => response.json())
-        .then(data => mostrarImagenes(data.hits));
+        .then(data => {
+            totalPaginas = calcularPaginas(data.totalHits);
+            mostrarImagenes(data.hits);
+        });
 }
 
 function mostrarImagenes(imagenes){
 
-    limpiarHtml();
+    limpiarHtml(true);
 
     //iterar sobre el arreglo de imagenes
     imagenes.forEach(imagen => {
@@ -44,17 +52,59 @@ function mostrarImagenes(imagenes){
             </div>
         `; 
     });
+
+    //limpiar paginador previo
+    limpiarHtml(false);
+
+    //generar paginador
+    imprimirPaginador();
 }
 
-function limpiarHtml(){
-    while(resultado.firstChild){
-        resultado.firstChild.remove();
+function imprimirPaginador(){
+
+    //-> FORMA DEL TEACHER XD
+    // iterador = crearPaginador(totalPaginas);
+    // while(true){
+    //     const { value, done } = iterador.next();
+    //     if(done) break;
+
+    //     //caso contrario
+    //     const boton = document.createElement('a');
+    //     boton.href = '#';
+    //     boton.dataset.pagina = value;
+    //     boton.textContent = value;
+    //     boton.classList.add('siguiente', 'bg-yellow-400', 'px-4', 'py-1', 'mr-2', 'font-bold', 'mb-4', 'uppercase', 'rounded');
+
+    //     paginacionDiv.appendChild(boton);
+    // }
+
+    //mi forma de crear paginadores
+    for(let i = 1; i <= totalPaginas; i++){
+        //caso contrario
+        const boton = document.createElement('a');
+        boton.href = '#';
+        boton.dataset.pagina = i;
+        boton.textContent = i;
+        boton.classList.add('siguiente', 'bg-yellow-400', 'px-4', 'py-1', 'mr-2', 'font-bold', 'mb-4', 'uppercase', 'rounded');
+        paginacionDiv.appendChild(boton);
+    }
+}
+
+function limpiarHtml(flag){
+    if(flag){
+        while(resultado.firstChild){
+            resultado.firstChild.remove();
+        }
+    }else{
+        while(paginacionDiv.firstChild){
+            paginacionDiv.firstChild.remove();
+        }
     }
 }
 
 function mostrarMensaje(msj){
 
-    limpiarHtml();
+    limpiarHtml(true);
 
     // const existeAlerta = document.querySelector('.bg-red-100');
 
@@ -70,6 +120,17 @@ function mostrarMensaje(msj){
         alertaHtml.remove();
     }, 3000);
 
+}
+
+//Genrador que va a registrar la cantidad de elementos de acuerdo a las páginas, parte de la forma del teacher
+// function *crearPaginador(total){
+//     for(let i = 1;i <= total; i++){
+//         yield i;
+//     }
+// }
+
+function calcularPaginas(total){
+    return parseInt(Math.ceil(total/registrosPagina));//->Math.ceil para redondear hacia arriba
 }
 
 //inicio
